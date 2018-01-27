537ebeb1f9d5ead1fc88c713e52e280063a378b3
TsHristov <tsvetanhristov@yahoo.com>
2018-01-27 22:38:49 +0200

    Fix decoding edge case

--- src/HuffmanTree.hs
+++ src/HuffmanTree.hs
@@ -7,7 +7,7 @@ data BinaryTree a = EmptyTree | Node { root  :: a,
                                        left  :: (BinaryTree a),
                                        right :: (BinaryTree a)
                                      } deriving (Show, Eq)
-
+  
 type Code            = String
 type CharFrequency   = (Char, Int)
 type CharFrequencies = [CharFrequency]
@@ -16,9 +16,9 @@ type HuffmanTree     = BinaryTree CharFrequency
 makeLeaf ::  CharFrequency -> HuffmanTree
 makeLeaf x = (Node x EmptyTree EmptyTree)
 
-isLeaf :: HuffmanTree -> Bool
-isLeaf (Node _ EmptyTree EmptyTree) = True
-isLeaf                              _  = False
+leaf :: HuffmanTree -> Bool
+leaf (Node _ EmptyTree EmptyTree) = True
+leaf                           _  = False
 
 -- Get character associated with root:
 char :: HuffmanTree -> Char
@@ -39,7 +39,7 @@ charFrequency (Node (_, x) _ _) (Node (_, y) _ _)
 -- Example: contentsFrequency "abracadabra" -> [('a',5),('b',2),('r',2),('c',1),('d',1)]
 contentsFrequencies :: String -> [CharFrequency]
 contentsFrequencies contents = nub $ map (\x -> (x, count x contents)) contents
-  where count x l = sum $ map (\y -> if y == x then 1 else 0) l
+  where count x l            = sum $ map (\y -> if y == x then 1 else 0) l
 
 -- Make each (Char,Frequency) pair a leaf in the Huffman Tree:
 makeLeaves :: CharFrequencies -> [HuffmanTree]
@@ -48,7 +48,7 @@ makeLeaves = map (\(x,y) -> makeLeaf (x, y))
 -- Merge two trees together, so that their frequencies sum up:
 mergeTrees :: HuffmanTree -> HuffmanTree -> HuffmanTree
 mergeTrees  leftTree rightTree = Node (' ', frequenciesSum) leftTree rightTree
-  where frequenciesSum = frequency leftTree + frequency rightTree
+  where frequenciesSum         = frequency leftTree + frequency rightTree
 
 -- Construct HuffmanTree based on file`s unique characters and their respective frequencies:
 constructHuffmanTree :: CharFrequencies -> HuffmanTree
@@ -69,29 +69,27 @@ constructHuffmanTree input    = construct $ makeLeaves input
 binaryCodes :: HuffmanTree -> [(Char, Code)]
 binaryCodes huffmanTree = generateCodes huffmanTree ""
   where generateCodes tree path
-          | isLeaf tree = [(char tree, path)]
-          | otherwise          = generateCodes (left tree) (path ++ "0")
-                                 ++ generateCodes (right tree) (path ++ "1")
+          | leaf tree   = [(char tree, path)]
+          | otherwise   = generateCodes (left tree) (path ++ "0")
+                          ++ generateCodes (right tree) (path ++ "1")
 
 -- Transform file`s contents to encoded sequence of 1`s and 0`s (Huffman Algorithm):
 -- Example: encodeContents "abracadabra" -> ("01111001100011010111100", <tree>),
 --          where <tree> is a valid Huffman Tree.
 encode :: String -> (Code, HuffmanTree)
-encode content            = (encoded content, huffmanTree)
-  where huffmanTree       = constructHuffmanTree $ contentsFrequencies content
-        encoded           = concat . binaryCode
+encode content      = (encoded content, huffmanTree)
+  where huffmanTree = constructHuffmanTree $ contentsFrequencies content
+        encoded     = concat . binaryCode
           where getValue (Just x) = x
                 codes             = Map.fromList $ binaryCodes $ huffmanTree
                 binaryCode        = Prelude.map (\x -> getValue (Map.lookup x codes))
 
 -- Decode a Huffman Tree in order to obtain file`s initial contents:
 decode :: (Code, HuffmanTree) -> String
-decode (code, huffmanTree)          = decodeTree code huffmanTree
-  where decodeTree [x] tree         = if x == '0' then [char $ left tree] else [char $ right tree]
-        decodeTree code@(x:xs) tree =
-          case x of '0' -> if isLeaf tree
-                           then char tree : decodeTree code huffmanTree
-                           else decodeTree xs (left tree)
-                    '1' -> if isLeaf tree
-                           then char tree : decodeTree code huffmanTree
-                           else decodeTree xs (right tree)
+decode (binaryCode, huffmanTree)                     = decodeTree binaryCode huffmanTree
+  where decodeTree "" EmptyTree                      = ""
+        decodeTree "" tree                           = [char tree]
+        decodeTree code (Node x EmptyTree EmptyTree) = fst x : decodeTree code huffmanTree
+        decodeTree (x:xs) tree                       = if x == '0'
+                                                       then decodeTree xs (left tree)
+                                                       else decodeTree xs (right tree)
